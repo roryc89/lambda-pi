47e7ebe68b4ef37a9adfaf2d53a8acf06c840a62
roryc89 <roryc89@googlemail.com>
2020-08-09 13:29:46 +0100

    adds type checking

--- src/LambdaPi/Main.hs
+++ src/LambdaPi/Main.hs
@@ -1,6 +1,7 @@
 module LambdaPi.Main where 
 
 -- import Data.Text
+import Control.Monad (unless)
 
 -- | Term with type produced by checking algorithm (Inferrable term)
 data TermUp
@@ -13,7 +14,7 @@ data TermUp
 -- | Term where type is an input to the checking algorithm (Checkable term)
 data TermDown 
     = Inf TermUp -- | Inferrable terms embedded in checkable terms
-    | Lamda TermDown
+    | Lambda TermDown
     deriving (Show, Eq)
 
 data Name 
@@ -23,19 +24,129 @@ data Name
     deriving (Show, Eq)
 
 data Type 
-    = Tree Name  -- | Type identifier
+    = TFree Name  -- | Type identifier
     | Fun Type Type -- | Arrow Function
     deriving (Show, Eq)
 
 data Value 
-    = VLam (Value -> Value) -- | Lambda abstraction
+    = VLambda (Value -> Value) -- | Lambda abstraction
     | VNeutral Neutral
 
--- | Neutral term 
+-- | Neutral term, i.e. a variable applied to a (possibly empty) sequence of values
 data Neutral
     = NFree Name 
-    | NApp Neutral Value
+    | NApply Neutral Value
     
 vfree :: Name -> Value 
 vfree n = VNeutral (NFree n)
 
+type Env = [Value]
+
+--------------------
+-- Evaluation 
+--------------------
+
+evalUp :: TermUp -> Env -> Value 
+evalUp t env = case t of 
+    Ann e _ -> evalDown e env
+    Free x -> vfree x
+    Bound i -> env !! i
+    Apply e1 e2 -> applyValue (evalUp e1 env) (evalDown e2 env)
+
+-- originally vapp 
+applyValue :: Value -> Value -> Value 
+applyValue v1 v2 = case v1 of 
+    VLambda f -> f v2
+    VNeutral n -> VNeutral (NApply n v2)
+
+evalDown :: TermDown -> Env -> Value 
+evalDown t env = case t of 
+    Inf i -> evalUp i env 
+    Lambda e -> VLambda (\x -> evalDown e (x : env))
+
+--------------------
+-- Contexts 
+--------------------
+
+data Kind = Star 
+    deriving (Show)
+
+data Info 
+    = HasKind Kind
+    | HasType Type 
+    deriving (Show)
+
+type Context = [(Name, Info)]
+
+--------------------
+-- Type Checking 
+--------------------
+
+type Result a = Either String a 
+
+throwError = Left
+
+kindDown :: Context -> Type -> Kind -> Result ()
+kindDown ctx t Star = case t of 
+
+    TFree x -> case lookup x ctx of 
+        Just (HasKind Star) -> return ()
+        Nothing -> throwError "unknown identifier"
+
+    Fun k1 k2 -> do 
+        kindDown ctx k1 Star 
+        kindDown ctx k2 Star 
+
+typeUp0 :: Context -> TermUp -> Result Type
+typeUp0 = typeUp 0 
+
+-- | Infer a term's type
+typeUp :: Int -> Context -> TermUp -> Result Type
+typeUp i ctx termU =  
+
+    case termU of 
+        Ann termD t -> do 
+            kindDown ctx t Star
+            return t
+
+        Free x -> case lookup x ctx of 
+            Just (HasType t) -> return t
+            Nothing -> throwError "unknown identifier"
+
+        Apply e1 e2 -> do 
+            fnType <- typeUp i ctx e1
+            case fnType of 
+                Fun t1 t2 -> do 
+                    typeDown i ctx e2 t1
+                    return t2
+                _ -> throwError "Cannot apply, not a function"
+
+-- | Check a term's type 
+typeDown :: Int -> Context -> TermDown -> Type -> Result ()
+typeDown i ctx termD suppliedType = case (termD, suppliedType) of 
+
+    (Inf termU, _) -> do 
+        inferredType <- typeUp i ctx termU 
+        unless (inferredType == suppliedType) (throwError "Type mismatch")
+
+    (Lambda lambdaTerm, Fun t1 t2) -> 
+        typeDown 
+            (i + 1) 
+            ((Local i, HasType t1) : ctx) 
+            (substDown 0 (Free (Local i)) lambdaTerm)
+            t2
+
+    tup -> throwError $ "type mismatch: " ++ show tup
+
+
+substUp :: Int -> TermUp -> TermUp -> TermUp 
+substUp i termU termUp_ = case termUp_ of 
+    Ann termD t -> Ann (substDown i termU termD) t 
+    Bound j -> if i == j then termU else Bound j
+    Free name -> Free name 
+    Apply e1 e2 -> Apply (substUp i termU e1) (substDown i termU e2)
+
+substDown :: Int -> TermUp -> TermDown -> TermDown
+substDown i termU termDown_ = case termDown_ of 
+    Inf e -> Inf (substUp i termU e)
+    Lambda e -> Lambda (substDown (i + 1) termU e)
